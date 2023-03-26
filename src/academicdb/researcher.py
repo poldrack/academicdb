71389f801ebd9648979e0e0c15ac9da06231bcbd
Russ Poldrack <poldrack@gmail.com>
2023-03-26 13:44:14 -0700

    remove get_patents

--- src/academicdb/researcher.py
+++ src/academicdb/researcher.py
@@ -9,48 +9,53 @@ import scholarly
 from scholarly import MaxTriesExceededException
 import pypatent
 from urllib.error import HTTPError
+try:
+    import tomllib
+except ModuleNotFoundError:
+    import tomli as tomllib
 
-from orcid import get_dois_from_orcid_record
-from pubmed import get_pubmed_data
-#from publication import JournalArticle, Book, BookChapter
-#from .crossref import get_crossref_records, parse_crossref_record
-from utils import get_additional_pubs_from_csv, CustomJSONEncoder, get_random_hash, drop_excluded_pubs
+from .orcid import get_dois_from_orcid_record
+from .pubmed import get_pubmed_data
+from .utils import get_additional_pubs_from_csv, CustomJSONEncoder, get_random_hash, drop_excluded_pubs
 
-
+researcher_fields = [
+    'scopus_id',
+    'orcid_data',
+    'dois',
+    'pubmed_data',
+    'crossref_data',
+    'gscholar_data',
+    'patent_data',
+    'serialized',
+    'publications',
+]
 
 class Researcher:
 
-    def __init__(self, param_file='params.json', basedir=None, format='latex'):
+    def __init__(self, param_file='config.toml', basedir=None):
         self.param_file = param_file
-        self.load_params(param_file)
         self.basedir = os.path.dirname(param_file) if basedir is None else basedir
-        self.format = format
-        fields_to_add = [
-            'scopus_id',
-            'orcid_data',
-            'orcid_dois',
-            'pubmed_data',
-            'crossref_data',
-            'gscholar_data',
-            'patent_data',
-            'serialized',
-            'publications',
-        ]
-        for field in fields_to_add:
+
+        self.setup_fields()
+        self.load_params()
+
+    def setup_fields(self):
+        for field in researcher_fields:
             if not hasattr(self, field):
                 setattr(self, field, None)
+    
 
-    def load_params(self, param_file):
-        if os.path.exists(param_file):
-            with open(param_file) as f:
-                params = json.load(f)
+    def load_params(self):
+        if os.path.exists(self.param_file):
+            with open(self.param_file, 'rb') as f:
+                params = tomllib.load(f)
         else:
-            raise FileNotFoundError("""Please create a json file called params.json
+            raise FileNotFoundError("""Please create a TOML file called config.toml
                                        containing the fields email (with your email address), orcid (with your ORCID id)
                                        and query (with your pubmed query)- see documentation for help')
                                        """)
-        for field in params:
-            setattr(self, field, params[field])
+        for field, value in params['researcher'].items():
+            setattr(self, field, value)
 
     def get_orcid_data(self, timeout=60):
         orcid_url = "https://pub.orcid.org/v3.0/%s" % self.orcid
@@ -66,13 +71,15 @@ class Researcher:
     def get_orcid_dois(self):
         if self.orcid_data is None:
             self.get_orcid_data()
-        self.orcid_dois = get_dois_from_orcid_record(self.orcid_data)
+        if self.dois is None:
+            self.dois = {}
+        self.dois['orcid'] = get_dois_from_orcid_record(self.orcid_data)
 
     def get_pubmed_data(self):
         self.pubmed_data = get_pubmed_data(self.query, self.email)
         print('retrieved %d full pubmed records' % len(self.pubmed_data['PubmedArticle']))
 
-    def get_google_scholar_record(self):
+    def get_google_scholar_data(self):
         try:
             search_query = scholarly.scholarly.search_author(
                 ' '.join([self.firstname, self.lastname]))
@@ -153,22 +160,6 @@ class Researcher:
                 print('skipping unknown type', additional_pubs[pub]['type'])
                 continue
             self.publications[pub].from_dict(additional_pubs[pub])
-
-    def get_patents(self):
-        try:
-            # this will fail if there are no patents
-            results = pypatent.Search(self.lastname).as_list()
-        except AttributeError:
-            print('pypatent failed to retrieve patents')
-            results = []
-
-        self.patent_data = []
-        for r in results:
-            for i in r['inventors']:
-                fn = i[0].split(' ')[0].lower()
-                ln = i[1].lower()
-                if fn == self.firstname.lower() and ln == self.lastname.lower():
-                    self.patent_data.append(r)
 
     def from_json(self, filename):
         with open(filename, 'r') as f:
