8d2ed5ac710b2007991f52b3b057a78c4cf6e9bd
Russ Poldrack <poldrack@gmail.com>
2023-03-26 12:20:24 -0700

    working version of mongodb creation

--- src/test/test_database.py
+++ src/test/test_database.py
@@ -2,8 +2,30 @@ import pytest
 import sys
 sys.path.append('../academicdb')
 from src.academicdb.database import AbstractDatabase
+from src.academicdb.database import MongoDatabase
+
+dbname = 'testdb'
+
+@pytest.fixture
+def mongodb():
+    db = MongoDatabase(dbname)
+    db.connect()
+    yield db
+    # clean up after tests are done
+    db.client.drop_database(dbname)
 
 # smoke test for AbstractDatabase
 def test_abstract_database():
     with pytest.raises(TypeError):
         AbstractDatabase()
+
+
+def test_mongo_creation(mongodb):
+    assert mongodb is not None
+
+
+def test_mongo_setup_db(mongodb):
+    mongodb.connect()
+    mongodb.setup_db()
+    mongodb.setup_collections()
+    assert dbname in mongodb.client.list_database_names()
