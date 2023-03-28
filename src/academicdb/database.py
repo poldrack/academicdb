1940b4490b9eafa58657bea1330b8c734bfa05f3
Russ Poldrack <poldrack@gmail.com>
2023-03-28 13:52:49 -0700

    clean up, add logging

--- src/academicdb/database.py
+++ src/academicdb/database.py
@@ -1,41 +1,6 @@
 from abc import ABC, abstractmethod
 import pymongo
-
-
-def foo():
-    client = pymongo.MongoClient(host="127.0.0.1", port=27017)
-    if dbname in client.list_database_names() and not overwrite:
-        # check to make sure only one metadata record exists
-        if len(list(client[dbname]['metadata'].find())) > 1:
-            raise ValueError(
-                "more than one metadata record exists in the database - please rerun with overwrite set to True")
-        return client[dbname]
-    elif dbname in client.list_database_names():
-        if collections is None:
-            client.drop_database(dbname)
-        else:
-            for c in collections:
-                client[dbname].drop_collection(c)
-    return setup_collections(client, dbname)
-
-
-def setup_collections(client: pymongo.mongo_client.MongoClient, dbname: str):
-    result = client[dbname]
-    collections_to_create = ['coauthors', 'funding', 'conferences', 'talks', 
-        'education', 'employment', 'distinctions', 'metadata', 'publications',
-        'invited_positions', 'memberships', 'service', 'trainees', 'pmcid']
-    for c in collections_to_create:
-        if c not in result.list_collection_names():
-            result.create_collection(c)
-    indices_to_create = {
-        'metadata': 'orcid',
-        'publications': 'eid',
-        'pmcid': 'pmid',
-    }
-    for c, idx in indices_to_create.items():
-        result[c].create_index([(idx, pymongo.ASCENDING)], unique=True)
-
-    return result
+import logging
 
 
 class AbstractDatabase(ABC):
@@ -79,17 +44,19 @@ class MongoDatabase(AbstractDatabase):
             if len(list(self.client[self.dbname]['metadata'].find())) > 1:
                 raise ValueError(
                     "more than one metadata record exists in the database - please rerun with overwrite set to True")
+            logging.debug("keeping existing database")
+
         # otherwise clean everything out and start over
         elif self.dbname in self.client.list_database_names():
+            logging.debug('dropping database')
             if self.collections is None:
                 for c in self.collections:
                     self.client[self.dbname].drop_collection(c)
             self.client[self.dbname].drop_database(self.dbname)
 
-        #self.client.create_database(self.dbname)
-
 
     def setup_collections(self, **kwargs):
+        logging.debug('setting up collections')
         result = self.client[self.dbname]
         for c in self.collections:
             if c not in result.list_collection_names():
@@ -100,9 +67,10 @@ class MongoDatabase(AbstractDatabase):
             'pmcid': 'pmid',
         }
         for c, idx in indices_to_create.items():
+            logging.debug(f'creating index {idx} on collection {c}')
             result[c].create_index([(idx, pymongo.ASCENDING)], unique=True)
 
-    def query(self):
+    def query(self, query_string: str, **kwargs):
         pass
 
 
