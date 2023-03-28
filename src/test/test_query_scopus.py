11b6244df2763e5e0994210752f0a18c3d1e6b1e
Russ Poldrack <poldrack@gmail.com>
2023-03-28 13:49:58 -0700

    initial add

---
+++ src/test/test_query_scopus.py
@@ -0,0 +1,15 @@
+import pytest
+import sys
+sys.path.append('../academicdb')
+from src.academicdb.query import ScopusQuery
+
+def test_single_record_returned():
+    scopus_search = ScopusQuery()
+    authorid = "7004739390" 
+    results = scopus_search.author_query(authorid)
+
+    assert len(results) > 300
+    for record in results:
+        # author names are truncated at 100 
+        if len(record.author_names.split(';')) < 100:
+            assert record.author_names.lower().find('poldrack') > -1
---
+++ src/test/test_researcher.py
@@ -0,0 +1,41 @@
+import pytest
+import sys
+import os
+myPath = os.path.dirname(os.path.abspath(__file__))
+sys.path.insert(0, os.path.join(myPath, '../academcicdb'))
+
+from src.academicdb.researcher import Researcher, researcher_fields
+
+
+@pytest.fixture
+def researcher():
+    return Researcher()
+
+
+def test_researcher_creation(researcher):
+    assert researcher is not None
+    assert researcher.orcid is not None
+    for field in researcher_fields:
+        assert hasattr(researcher, field)
+ 
+def test_orcid_data(researcher):
+    researcher.get_orcid_data()
+    assert researcher.orcid_data is not None
+    assert researcher.orcid_data['orcid-identifier']['path'] == researcher.orcid
+    assert 'activities-summary' in researcher.orcid_data
+    assert 'works' in researcher.orcid_data['activities-summary']
+
+    researcher.get_orcid_dois()
+    assert researcher.dois['orcid'] is not None
+
+def test_pubmed_data(researcher):
+    researcher.get_pubmed_data()
+    assert researcher.pubmed_data is not None
+    assert 'PubmedArticle' in researcher.pubmed_data
+    assert len(researcher.pubmed_data['PubmedArticle']) > 250
+
+# skip this test for now, because it takes a long time to run and is currently working
+# def test_google_scholar(researcher):
+#     researcher.get_google_scholar_data()
+#     assert researcher.gscholar_data is not None
+
