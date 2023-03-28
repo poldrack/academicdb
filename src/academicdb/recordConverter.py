0f906ba51dcc95b0515018ec54bf02d759898c63
Russ Poldrack <poldrack@gmail.com>
2023-03-28 13:49:19 -0700

    moving to use crossref for scopus conversion

--- src/academicdb/recordConverter.py
+++ src/academicdb/recordConverter.py
@@ -1,6 +1,11 @@
 ## create an abstract base class for record conversion
 from abc import ABC, abstractmethod
-from .pubmed import parse_pubmed_record
+from pybliometrics.scopus import AbstractRetrieval
+from crossref.restful import Works
+from . import query # import PubmedQuery
+from . import pubmed # import parse_pubmed_record
+from . import crossref_utils
+import logging
 
 
 class AbstractRecordConverter(ABC):
@@ -15,18 +20,53 @@ class AbstractRecordConverter(ABC):
         """
         pass
     
-# create a converter for pubmed records
 
 class PubmedRecordConverter(AbstractRecordConverter):
     """
     """
     def __init__(self, record):
         super().__init__(record)
-        self.pub_type = 'pubmed'
 
     def convert(self):
         """
         """
-        self.pub = parse_pubmed_record(self.record)
+        self.pub = pubmed.parse_pubmed_record(self.record)
+        return(self.pub)
+    
+
+class ScopusRecordConverter(AbstractRecordConverter):
+    """
+    """
+    def __init__(self, record, email):
+        super().__init__(record)
+        self.email = email
+
+    def convert(self):
+        """
+        """
+        crossref_record = Works().doi(self.record.doi)
+        pubmed_record = query.PubmedQuery(email=self.email).query(self.record.doi)
+        if pubmed_record is None:
+            return None
+        # TODO - deal with multiple pubmed records per doi
+        if len(pubmed_record) > 1:
+            logging.warning(f'Multiple pubmed records found for doi: {self.record.doi}')
+    
+        self.pub = pubmed.parse_pubmed_record(pubmed_record[0])
+        self.pub['scopus_coauthor_ids'] = self.record.author_ids.split(';')
+        return(self.pub)
+
+    
+
+class CrossrefRecordConverter(AbstractRecordConverter):
+    """
+    """
+    def __init__(self, record):
+        super().__init__(record)
+
+    def convert(self):
+        """
+        """
+        self.pub = crossref_utils.parse_crossref_record(self.record)
         return(self.pub)
     
