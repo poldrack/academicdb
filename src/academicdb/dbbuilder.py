92860ee43a3c74499b74a059d35271760e046875
Russ Poldrack <poldrack@gmail.com>
2023-03-31 15:13:35 -0700

    add get_coauthors

--- src/academicdb/dbbuilder.py
+++ src/academicdb/dbbuilder.py
@@ -127,6 +127,41 @@ def setup_db(dbconfigfile, overwrite):
         return database.Database(database.MongoDatabase(overwrite=overwrite))
 
 
+def get_affiliation(aff):
+    if aff.parent_preferred_name is not None:
+        return f'{aff.preferred_name}, {aff.parent_preferred_name}, {aff.city}, {aff.country}'
+    else:
+        return f'{aff.preferred_name}, {aff.city}, {aff.country}'
+
+def get_coauthors(publications):
+
+    coauthors = {}
+    for pub in publications:
+        if 'scopus_coauthor_ids' in pub:
+            for coauthor in pub['scopus_coauthor_ids']:
+                if coauthor not in coauthors:
+                    coauthor_info = AuthorRetrieval(coauthor)
+                    if coauthor_info.indexed_name is None:
+                        continue
+                    if coauthor_info.affiliation_current is None:
+                        affil = None
+                        affil_id = None
+                    else:
+                        affil = [get_affiliation(aff) for aff in coauthor_info.affiliation_current]
+                        affil_id = [aff.id for aff in coauthor_info.affiliation_current]
+                    coauthors[coauthor] = {
+                        'scopus_id': coauthor,
+                        'name': coauthor_info.indexed_name,
+                        'affiliation': affil,
+                        'affiliation_id': affil_id,
+                        'year': pub['year']
+                    }
+                else:
+                    if pub['year'] > coauthors[coauthor]['year']:
+                        coauthors[coauthor]['year'] = pub['year']
+    return coauthors
+
+
 def main():
     
     args = parse_args()
@@ -213,5 +248,7 @@ def main():
 
     r.publications = add_citations(r.publications)
 
+    r.get_coauthors()
+    
     if not args.nodb:
         r.to_database(db)
