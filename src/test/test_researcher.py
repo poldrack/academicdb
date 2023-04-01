480e607c9c5d59471966f98fb648c5b072f2075d
Russ Poldrack <poldrack@gmail.com>
2023-04-01 08:45:57 -0700

    use temp config file

--- src/test/test_researcher.py
+++ src/test/test_researcher.py
@@ -2,15 +2,52 @@ import pytest
 import sys
 import os
 
+try:
+    import tomllib
+except ModuleNotFoundError:
+    import tomli as tomllib
+import tomli_w
+
 myPath = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, os.path.join(myPath, '../academcicdb'))
 
 from src.academicdb.researcher import Researcher, researcher_fields
 
 
+@pytest.fixture(scope='session')
+def config_file(tmp_path_factory):
+    config = {
+        'researcher': {
+            'lastname': 'poldrack',
+            'middlename': 'alan',
+            'firstname': 'russell',
+            'email': 'russ@nowhere.edu',
+            'orcid': '0000-0001-6755-0259',
+            'query': 'poldrack-r',
+            'url': 'http://poldrack.github.io',
+            'twitter': '@russpoldrack',
+            'github': 'http://github.com/poldrack',
+            'phone': '650-497-8488',
+            'scholar_id': 'RbmLvDIAAAAJi',
+            'scopus_id': '7004739390',
+            'address': [
+                'Stanford University',
+                'Department of Psychology',
+                'Building 420',
+                '450 Jane Stanford Way',
+                'Stanford, CA, 94305-2130',
+            ],
+        }
+    }
+    fn = tmp_path_factory.mktemp('data') / 'config.toml'
+    with open(fn, 'wb') as f:
+        tomli_w.dump(config, f)
+    return fn
+
+
 @pytest.fixture
-def researcher():
-    return Researcher()
+def researcher(config_file):
+    return Researcher(config_file)
 
 
 def test_researcher_creation(researcher):
